f89e535e7378ac6d6837533a37caaae0cb46fada
へマンタ <hghimire@asahimarkets.co.jp>
2025-12-05 15:54:57 +0900

    g

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true, // App Router सक्षम पार्न
-  },
-  images: {
-    domains: [], // यदि बाहिरी images राख्न चाहानुहुन्छ भने यहाँ domain राख्नुहोस्
+  webpack(config) {
+    config.resolve.alias['@'] = require('path').resolve(__dirname, 'app');
+    return config;
   },
 };
 
